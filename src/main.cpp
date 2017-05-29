af4e4ad0a657d7e8f386073258ad2c8f7a1961a4
guillaume <guillaum324bis@hotmail.fr>
2017-05-29 16:26:46 +0200

    split main

--- src/main.cpp
+++ src/main.cpp
@@ -17,27 +17,32 @@ using namespace std;
 using namespace cv;
 using namespace raspicam;
 
-int main()
+void creatSemephores()
 {
-	/*creation des semaphores*/
 	if(rt_sem_create(&semConnecterServeur, "semConnecterServeur", 0, S_PRIO) != 0)
-    	perror("erreur lors de la creation du sem ConnecterServeur\n");
-    if(rt_sem_create(&semConnecterRobot, "semConnecterRobot", 0, S_PRIO) != 0)
-        perror("erreur lors de la creation du sem sConnecterRobot\n");
+		perror("erreur lors de la creation du sem ConnecterServeur\n");
+	if(rt_sem_create(&semConnecterRobot, "semConnecterRobot", 0, S_PRIO) != 0)
+		perror("erreur lors de la creation du sem sConnecterRobot\n");
+}
 
-    /*creation des mutex*/
-    if(rt_mutex_create(&mutexEtat, "mutexEtat") != 0)
-    	perror("erreur lors de la creation du mutex mutexEtat\n");
-    if(rt_mutex_create(&mutexMove, "mutexMove") != 0)
-       	perror("erreur lors de la creation du mutex mutexMove\n");
+void creatMutex()
+{
+	if(rt_mutex_create(&mutexEtat, "mutexEtat") != 0)
+		perror("erreur lors de la creation du mutex mutexEtat\n");
+	if(rt_mutex_create(&mutexMove, "mutexMove") != 0)
+		perror("erreur lors de la creation du mutex mutexMove\n");
+}
 
-    /*creation des queues*/
-    if(rt_queue_create(&queueMsgGUI, "queueMsgGUI", 30, Q_UNLIMITED, Q_PRIO) != 0)
-    	perror("erreur lors de la creation de la queue queueMsgGUI\n");
-    if(rt_queue_create(&queueMsgRobot, "queueMsgRobot", 20, Q_UNLIMITED, Q_PRIO) != 0)
-        perror("erreur lors de la creation de la queue queueMsgRobot\n");
+void creatQueues()
+{
+	if(rt_queue_create(&queueMsgGUI, "queueMsgGUI", 30, Q_UNLIMITED, Q_PRIO) != 0)
+		perror("erreur lors de la creation de la queue queueMsgGUI\n");
+	if(rt_queue_create(&queueMsgRobot, "queueMsgRobot", 20, Q_UNLIMITED, Q_PRIO) != 0)
+		perror("erreur lors de la creation de la queue queueMsgRobot\n");
+}
 
-    /*creation des taches*/
+void spawnTasks()
+{
 	if(rt_task_spawn(&tServeur, "serveur", 0, PRIORITY_TSERVEUR, T_JOINABLE, &threadServeur, NULL) != 0)
 		perror("erreur lors de la creation du thread Serveur\n");
 	if(rt_task_spawn(&tvideo,"tvideo", 0, PRIORITY_TVIDEO, T_JOINABLE, &threadVideo, NULL) != 0)
@@ -53,34 +58,62 @@ int main()
 	if(START_MODE == WITH_WD)
 		if(rt_task_spawn(&tWatchdog, "tWatchdog", 0, PRIORITY_TWATCHDOG, T_JOINABLE, &threadWatchdog, NULL) != 0)
 			perror("erreur lors de la creation du thread wdg\n");
+}
 
-    rt_task_join(&tServeur);
-    rt_task_join(&tvideo);
-    rt_task_join(&tconnect);
-    rt_task_join(&tenvoyer);
-    rt_task_join(&tmove);
-    rt_task_join(&tbattery);
+void joinTasks()
+{
+	rt_task_join(&tServeur);
+	rt_task_join(&tvideo);
+	rt_task_join(&tconnect);
+	rt_task_join(&tenvoyer);
+	rt_task_join(&tmove);
+	rt_task_join(&tbattery);
+}
 
-    /*destruction des taches*/
-    rt_task_delete(&tServeur);
-    rt_task_delete(&tvideo);
-    rt_task_delete(&tconnect);
-    rt_task_delete(&tenvoyer);
-    rt_task_delete(&tmove);
-    rt_task_delete(&tbattery);
-    rt_task_delete(&tWatchdog);
+void deleteTasks()
+{
+	rt_task_delete(&tServeur);
+	rt_task_delete(&tvideo);
+	rt_task_delete(&tconnect);
+	rt_task_delete(&tenvoyer);
+	rt_task_delete(&tmove);
+	rt_task_delete(&tbattery);
+	rt_task_delete(&tWatchdog);
+}
 
-    /*destruction des mutex*/
-    rt_mutex_delete(&mutexEtat);
-    rt_mutex_delete(&mutexMove);
+void deleteMutex()
+{
+	rt_mutex_delete(&mutexEtat);
+	rt_mutex_delete(&mutexMove);
+}
 
-    /*destruction des semaphores*/
-    rt_sem_delete(&semConnecterServeur);
-    rt_sem_delete(&semConnecterRobot);
+void deleteSemaphores()
+{
+	rt_sem_delete(&semConnecterServeur);
+	rt_sem_delete(&semConnecterRobot);
+}
 
-    /*destruction de la queue*/
-    rt_queue_delete(&queueMsgGUI);
-    rt_queue_delete(&queueMsgRobot);
+void deleteQueues()
+{
+	rt_queue_delete(&queueMsgGUI);
+	rt_queue_delete(&queueMsgRobot);
+}
+
+int main()
+{
+	creatSemephores();
+	creatMutex();
+	creatQueues();
+
+	spawnTasks();
+
+	joinTasks();
+
+	deleteTasks();
+
+	deleteMutex();
+	deleteSemaphores();
+	deleteQueues();
 
     printf("end main \n");
     return 0;
